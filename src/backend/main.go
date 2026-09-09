687658d2e56b2a08701c34dda0e5f88ffcb1d1fb
YacqubJAD <yadi0001@stud.kea.dk>
2026-09-09 22:57:18 +0200

    feat: added GET-methode for the register-page(register.html)

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +13,15 @@ func main() {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
+		templates.ExecuteTemplate(w, "search.html", nil)
+		})
+
+	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		templates.ExecuteTemplate(w, "register.html", nil)
 	})
 
 	http.ListenAndServe(":8080", nil)
