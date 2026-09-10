b605ffed4be6eb6fddda098d9b7c0bcb5a6449d7
Marco <m.stormly@yahoo.dk>
2026-09-10 21:51:27 +0200

    feat: Setup api search route, missing page route and db connection

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Hello, world!")
+func main() {
+	//fmt.Println("Hello, world!")
+	router()
+}
+
+func router() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /api/search", getSearch)
+	router.HandleFunc("POST /api/register", postRegister)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getSearch(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query().Get("q")
+	_ = q // Use the query parameter 'q' as needed
+	language := r.URL.Query().Get("language")
+	if language == "" {
+		language = "en"
+	}
+
+	response := map[string]any{
+		"search_results": []string{},
+		//Data suppose to be written here
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func postRegister(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Register endpoint hit")
 }
